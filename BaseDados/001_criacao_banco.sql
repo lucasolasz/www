62f36be7e3746f6d7b0f234af7cd2872a37ca0d4
conuaz <lucas.olasz@tty2000.com.br>
2025-10-03 11:56:50 -0300

    Remoção de itens que não preciso

--- BaseDados/001_criacao_banco.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `projeto_base` CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `projeto_base`;
--- app/config/configuracao.php
+++ app/config/configuracao.php
@@ -1,18 +1,9 @@
 <?php 
-
-//Parametros para o Banco
-define('DB_HOST', 'db');
-define('DB_USER', 'root');
-define('DB_PASS', '123');
-define('DB_NAME', 'projeto_base');
-define('DB_PORT', '3306');
-define('DB_CHARSET', 'utf8');
-
 //dirname() retorna o nome do diretorio em que o arquivo está
 define('APP', dirname(dirname(__FILE__)));
 
 define('URL', '/');
 
-define('APP_NOME', 'Projeto Base');
+define('APP_NOME', 'Leauto');
 
 define('APP_VERSAO', '1.0.0');
