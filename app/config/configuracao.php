dc7b9caefbd2ea3f5fb8c6612e276e229d728490
conuaz <lucas.olasz@tty2000.com.br>
2025-10-03 16:38:21 -0300

    Inclui whatsapp

--- app/config/configuracao.php
+++ app/config/configuracao.php
@@ -6,6 +6,8 @@ define('VIEWS', APP . '/views/');
 define('SECOES', VIEWS . 'paginas/secoes/');
 define('COMPONENTES', VIEWS . 'components/');
 
+define('URL_WHATSAPP', 'https://api.whatsapp.com/send?phone=5521998063440&text=Ol%C3%A1%2C%20gostaria%20de%20conversar%20com%20um%20atendente.%20');
+
 define('URL', '/');
 
 define('APP_NOME', 'Leauto');
