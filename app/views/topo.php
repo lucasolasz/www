fd24148cda865598ee7e2f581529798ee4945395
conuaz <lucas.olasz@tty2000.com.br>
2025-10-03 12:46:30 -0300

    Ajusta o nav

--- app/views/topo.php
+++ app/views/topo.php
@@ -6,29 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Oswald:400,300,700" rel="stylesheet" type="text/css">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
-
-    <!-- DataTables CSS -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.css" />
-
     <!-- Custom Styles -->
     <link href="<?php echo URL . 'public/css/estilos.css' ?>" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
 
     <title><?php echo APP_NOME; ?></title>
-
-    <!-- jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-    <!-- Bootstrap Bundle JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-
-    <!-- DataTables JS -->
-    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.js"></script>
 
     <!-- Custom Scripts -->
     <script src="<?php echo URL . 'public/js/jquery.funcoes.js' ?>"></script>
@@ -36,57 +21,84 @@
 
 <body>
 
+    <header>
 
+        <nav class="bg-black text-white p-4">
+            <div class="flex justify-between items-center max-w-7xl mx-auto">
 
-    <header class="bg-light">
+                <!-- Logo -->
+                <div class="cursor-pointer">
+                    <a href="https://www.renault.com.br/">
+                        <img
+                            src="/assets/logo-renault.jpg"
+                            alt="Logo da Empresa LEAUTO"
+                            width="80"
+                            height="40"
+                            class="w-20 sm:w-40 lg:w-20 h-auto" />
+                    </a>
+                </div>
 
-        <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="<?= URL ?>"><?= APP_NOME ?></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" aria-current="page" href="<?= URL ?>">Home</a>
-                            </li>
-                            <?php if (isset($_SESSION['id_usuario'])) { ?>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
-                                        aria-expanded="false">Cadastro</a>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" aria-current="page"
-                                                href="<?= URL . 'Usuarios/visualizarUsuarios' ?>">Usuário</a></li>
-                                    </ul>
-                                </li>
-                            <?php } ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="<?= URL . 'Paginas/sobre' ?>">Sobre nós</a>
-                            </li>
-                        </ul>
-                        <div class="d-flex align-items-center">
-                            <?php if (isset($_SESSION['id_usuario'])) { ?>
-                                <div class="me-2" style="width: 300px;">
-                                    <p class="mb-0">Olá, <?= ucfirst($_SESSION['ds_nome_usuario']); ?>, Seja bem vindo(a)
-                                    </p>
-                                </div>
-                                <div>
-                                    <a class="btn btn-sm btn-danger" href="<?= URL . 'Usuarios/sair' ?>">Sair</a>
-                                </div>
-                            <?php } else { ?>
-                                <div style="width: 30px">
-                                    <a class="btn btn-primary" href="<?= URL . 'Usuarios/login' ?>">Entrar</a>
-                                </div>
-                            <?php } ?>
-                        </div>
-
+                <!-- Links do menu (desktop) -->
+                <div class="hidden md:flex flex-grow justify-center">
+                    <div class="flex items-center space-x-6">
+                        <a href="#ofertas" class="p-2 font-semibold hover:text-gray-400">Ofertas</a>
+                        <a href="#unidades" class="p-2 font-semibold hover:text-gray-400">Lojas</a>
+                        <a href="#quemsomos" class="p-2 font-semibold hover:text-gray-400">Quem somos</a>
                     </div>
                 </div>
-            </nav>
-        </div>
+
+                <!-- Botão de cotação (desktop) -->
+                <div class="hidden md:block">
+                    <button class="bg-[#EFDF00] h-10 px-4 cursor-pointer text-black font-semibold hover:bg-black hover:border hover:text-white active:border transition-colors">
+                        Peça uma cotação
+                    </button>
+                </div>
+
+                <!-- Ícone do menu (mobile) -->
+                <div class="block md:hidden z-50">
+
+                    <!-- Checkbox oculto para controlar estado -->
+                    <input type="checkbox" id="menu-toggle" class="hidden peer" />
+
+                    <!-- Botão hamburguer/fechar -->
+                    <!-- Botão de abrir/fechar -->
+                    <label for="menu-toggle" class="cursor-pointer text-white text-2xl z-50">
+                        <!-- Ícone menu (hambúrguer) -->
+                        <i class="fa-solid fa-bars block peer-checked:hidden"></i>
+                        <!-- Ícone fechar (X) -->
+                        <!-- <i class="fa-solid fa-xmark hidden peer-checked:block"></i> -->
+                    </label>
+
+                    <!-- Menu lateral mobile -->
+                    <div
+                        class="fixed left-[-100%] top-0 w-[60%] h-full bg-black border-r border-gray-900 ease-in-out duration-500 z-40 
+               peer-checked:left-0">
+                        <div class="p-4 mt-20">
+                            <ul>
+                                <li class="p-4 border-b border-gray-600">
+                                    <a href="#ofertas" class="font-semibold hover:text-gray-400">Ofertas</a>
+                                </li>
+                                <li class="p-4 border-b border-gray-600">
+                                    <a href="#unidades" class="font-semibold hover:text-gray-400">Lojas</a>
+                                </li>
+                                <li class="p-4 border-b border-gray-600">
+                                    <a href="#quemsomos" class="font-semibold hover:text-gray-400">Quem somos</a>
+                                </li>
+                                <li class="mt-4">
+                                    <button
+                                        class="font-semibold w-full h-10 bg-[#EFDF00] text-center p-2 text-black hover:bg-black hover:text-white active:border active:bg-black active:text-white cursor-pointer">
+                                        Peça uma cotação
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
 
     </header>
